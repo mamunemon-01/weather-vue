e17c96ec91c8a5d7b1eeb39178c43b4c189fa676
Mamun Al Imran <sciemontist@gmail.com>
2025-12-26 21:33:24 +0600

    Default elements have been replaced with new elements in the app.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <div class="header container h-100 p-5 mt-2">
+      <h1 class="text-center mb-5">Weather Vue</h1>
+      <div class="d-flex justify-content-center h-100">
+        <div class="searchbar w-50 mx-2">
+          <input type="text" class="input form-control" placeholder="Enter a City Name">
+        </div>
+        <button class="btn btn-primary btn-search">Search <i class="fas fa-search"></i></button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    background-color: #121212 !important;
+  }
+  .header {
+    background-color: #212730;
+    color: #ffffff;
+    border-radius: 20px;
+  }
+  .btn-search {
+    background-image: linear-gradient(to right, cyan, magenta);
+  }
 </style>
