ebbffb01e0cb472064edc34e378a7b1dced73e56
Mamun Al Imran <sciemontist@gmail.com>
2025-12-27 03:24:08 +0600

    The OpenWeatherMap API has been integrated and showing of some data except for the day-based forecasts has been enabled.

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -3,13 +3,13 @@
     <div class="d-flex mb-2">
       <div class="card main-div w-100">
         <div class="p-3">
-          <h2 class="mb-1 day">Tuesday</h2>
-          <p class="text-light date">date</p>
-          <small>time</small>
-          <h2 class="place"><i class="fas fa-location"></i> Rio <small>Country</small></h2>
+          <h2 class="mb-1 day">Today</h2>
+          <p class="text-light date">{{date}}</p>
+          <small>{{time}}</small>
+          <h2 class="place"><i class="fas fa-location me-1"></i> {{ cityName }}, <small>{{country}}</small></h2>
           <div class="temp">
-            <h1 class="weather-temp">19&deg;</h1>
-            <h2 class="text-light">description</h2>
+            <h1 class="weather-temp">{{temperature}}&deg;</h1>
+            <h2 class="text-light">{{description}} <img :src="iconUrl" alt="Weather Icon" class="bg-light rounded rounded-2"></h2>
           </div>
         </div>
       </div>
@@ -17,16 +17,16 @@
         <table class="m-4">
           <tbody>
             <tr>
-              <th>Sea Level</th>
-              <td>100</td>
+              <th>Humidity</th>
+              <td>{{humidity}}%</td>
+            </tr>
+            <tr>
+              <th>Wind Speed</th>
+              <td>{{windSpeed}} m/s</td>
             </tr>
             <tr>
               <th>Sea Level</th>
-              <td>100</td>
-            </tr>
-            <tr>
-              <th>Sea Level</th>
-              <td>100</td>
+              <td>{{seaLevel}}</td>
             </tr>
           </tbody>
         </table>
@@ -42,7 +42,8 @@
 </template>
 
 <script>
-import DayWeather from './DayWeather.vue'
+import DayWeather from './DayWeather.vue';
+import axios from 'axios';
 
 export default {
   name: 'theWeather',
@@ -54,7 +55,36 @@ export default {
   },
   data() {
     return {
+      temperature: null,
+      description: null,
+      iconUrl: null,
+      date: null,
+      time: null,
+      humidity: null,
+      windSpeed: null,
+      seaLevel: null,
+      cityName: null,
+      country: null,
     }
+  },
+  async created() {
+    const apiString = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=21bfb338ac807e5f3c77d75cf67e65fc`;
+    // console.log(apiString);
+    const response = await axios.get(apiString);
+    const weatherData = response.data;
+    console.log(weatherData);
+    this.temperature = Math.round(weatherData.main.temp);
+    this.description = weatherData.weather[0].description;
+    this.iconUrl = `http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    // console.log(this.iconUrl);
+    this.date = new Date();
+    this.time = this.date.toLocaleTimeString();
+    this.date = this.date.toLocaleDateString();
+    this.humidity = weatherData.main.humidity;
+    this.windSpeed = weatherData.wind.speed;
+    this.seaLevel = weatherData.main.sea_level || 'N/A';
+    this.cityName = weatherData.name;
+    this.country = weatherData.sys.country;
   }
 }
 </script>
